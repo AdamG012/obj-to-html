e0fb582c2bea9f3158e2daccad79ec980ba21461
Adam G <61303668+AdamG012@users.noreply.github.com>
2023-01-28 22:56:23 +1100

    feat: Included documentation and separated methods

--- obj_html_converter/utils.py
+++ obj_html_converter/utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+from os.path import exists, dirname, abspath
 import requests
 
 
@@ -13,16 +14,68 @@ def get_config_dir():
     return home_path
 
 
-def grab_canvas_urls(file_name, access_token, course_num, prefix):
-    if file is None or len(files) == 0:
-        raise ValueError("Invalid configuration, no files entered.")
+def convert_mtl_file(mtl_file, access_token, directory, prefix=None, course_num=None):
+    # Change the current path to be relative to the MTL file
+    # This is done so that users don't have to go into the file and
+    # modify the paths to be relative to the working direcotry
+    curr_path = abspath(getcwd())
+    chdir(dirname(abspath(mtl_file)))
 
-    request = requests.get("https://{}/api/v1/{}files/{}?access_token={}".format(prefix, course_num, file_name, access_token))
-    if request is None or 'url' not in request.json():
-            raise ValueError(f"Invalid configuration for {f}, check your arguments.\nGenerated request:\n{request}")
+    # Update the MTL file with the link of the texture file
+    mtl_raw = None
+    with open(mtl_file, 'r') as f:
+        mtl_raw = f.readlines()
 
-    url    = request.json()['url']
-    return url
+    # Find the unique lines mapping to the texture files
+    map_kds = {}
+
+    # Loop over finding lines that match the texture files
+    for i, line in enumerate(mtl_raw):
+        if "map_Kd" in line:
+            text_url = None
+            line = line.strip()
+
+            # If not in the map then autoconvert the link
+            if line not in map_kds:
+                texture_path = line.split(" ")[1]
+                text_url = file_to_link(file_path=texture_path, access_token=access_token,
+                        directory=directory, prefix=prefix, course_num=course_num)
+                map_kds[line] = text_url
+
+            # If we have converted this link before
+            else:
+                text_url = map_kds[line]
+            mtl_raw[i] = f"map_Kd {text_url}\n"
+
+    # To not overwrite the existing file
+    mtl_file = mtl_file[:-4] + "_online.mtl"
+    with open(mtl_file, 'w') as f:
+        f.write("".join(mtl_raw))
+
+    # Change the directory back to the working DIR
+    chdir(abspath(curr_path))
+
+    # Convert MTL File
+    return file_to_link(file_path=mtl_file, access_token=access_token, directory=directory, prefix=prefix, course_num=course_num)
+
+
+def autoconvert_files(obj_file, texture_file, mtl_file, access_token, directory, prefix=None, course_num=None):
+    # Convert OBJ
+    obj_url = file_to_link(file_path=obj_file, access_token=access_token,
+                           directory=directory, prefix=prefix, course_num=course_num)
+    texture_url = None
+    mtl_url = None
+
+    # Convert Texture File
+    if mtl_file is None and texture_file is not None:
+        texture_url = file_to_link(file_path=texture_file, access_token=access_token,
+                                   directory=directory, prefix=prefix, course_num=course_num)
+
+    # Convert the MTL File
+    if mtl_file is not None:
+        mtl_url = convert_mtl_file(mtl_file, access_token, directory, prefix, course_num)
+
+    return obj_url, texture_url, mtl_url
 
 
 def file_to_link(file_path, access_token, directory, prefix, course_num=None):
@@ -37,7 +90,7 @@ def file_to_link(file_path, access_token, directory, prefix, course_num=None):
     - course_num        : the course number if uploading to a specific course
     """
 
-    # TODO Warning this will overwrite existing files uploaded
+    # NOTE Warning this will overwrite existing files uploaded
     file_name=os.path.basename(file_path)
 
     # Set the course path to the users files or to a course's files
@@ -46,7 +99,6 @@ def file_to_link(file_path, access_token, directory, prefix, course_num=None):
         course_path = "users/self/"
     else:
         course_path = f"courses/{course_num}/"
-
 
     # Step 1: Notify Canvas of the file upload
     params = {"parent_folder_path": directory,
@@ -73,10 +125,6 @@ def file_to_link(file_path, access_token, directory, prefix, course_num=None):
     return upload_json['url']
 
 
-def upload_file(file_name, access_token, course_num, prefix):
-    pass
-
-
 def save_token(access_token, prefix):
 
     # Check if invalid
@@ -98,6 +146,7 @@ def save_token(access_token, prefix):
 
 def load_token(prefix):
 
+    # Check if invalid
     if prefix is None:
         raise ValueError("Prefix cannot be none!")
 
